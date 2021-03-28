84096967b4b86ddfcfc9c34a8f2cb5faca701da6
Mustafa Ömer ESER <mustafaomereser@gmail.com>
2021-03-28 20:43:36 +0300

    DB update

--- modules/DB.php
+++ modules/DB.php
@@ -28,8 +28,8 @@ class DB
     public function where($where)
     {
         foreach ($where as $key => $val) {
-            $this->data['where'][] = $val[0] . " " . $val[1] . " :" . $val[0];
-            $this->params[":" . $val[0]] = $val[2];
+            $this->data['where'][] = $val[0] . " " . $val[1] . " :where_" . $val[0];
+            $this->params["where_" . $val[0]] = $val[2];
         }
 
         return $this;
@@ -54,10 +54,46 @@ class DB
         return $this;
     }
 
+    public function create($data = [])
+    {
+        $this->sql = "INSERT INTO $this->table(" . implode(", ", array_keys($data)) . ") VALUES(:" . implode(", :", array_keys($data)) . ")";
+        $ex = $this->ezPDO->exec($this->sql, $data);
+
+        if ($ex)
+            return $this->ezPDO->read("SELECT * FROM $this->table WHERE id = :id", ["id" => $this->ezPDO->lastID()]);
+
+
+        return false;
+    }
+
+    public function update($data = [])
+    {
+
+        $update_sql = "";
+
+        foreach ($data as $key => $val) {
+            $update_sql .= "$key = :$key, ";
+        }
+
+        $update_sql = rtrim($update_sql, ', ');
+
+        $where_ = $this->build();
+
+        $data = array_merge($this->params, $data);
+
+        $sql = "UPDATE $this->table SET $update_sql" . $where_;
+
+        $ex = $this->ezPDO->exec($sql, $data);
+
+        if ($ex)
+            return $this->ezPDO->read("SELECT * FROM $this->table " . $where_, $this->params);
+
+        return false;
+    }
+
     public function get($fields = "*")
     {
         $this->sql = "SELECT $fields FROM $this->table $this->sql";
-
         return $this->ezPDO->read($this->build(), $this->params);
     }
 }
